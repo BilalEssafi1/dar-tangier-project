7f6bf2ea11d7939efee4aca3f0d9e30577f54f32
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-10-26 16:17:49 +0000

    Set secret key

--- dartangier/settings.py
+++ dartangier/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0#87=d-uvn=gicgpzt9w5l@sw29=g_*=r_e*b%*5)ry=s)m^g)'
+# Secret key
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
