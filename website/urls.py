1519d116e32ab5d5976b99323140ee29d680fa26
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-10-29 09:53:45 +0000

    Add content for location page

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='index'),
     path('menu/', views.menu, name='menu'),
-    path('location/', views.menu, name='location')
+    path('location/', views.location, name='location')
     ]
