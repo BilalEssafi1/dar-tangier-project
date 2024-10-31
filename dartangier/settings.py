f531cad30b4c7926d110bcadeac49491947e6f68
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-10-31 15:44:33 +0000

    Removed unnecessary social authentication.

--- dartangier/settings.py
+++ dartangier/settings.py
@@ -29,7 +29,7 @@ GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-bilalessafi-dartangierp-bjz8u5exeeo.ws.codeinstitute-ide.net'
                 ,'.herokuapp.com']
