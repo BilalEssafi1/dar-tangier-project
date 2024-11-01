a44233d3618361d66614fe1ed7efafdfde2f642a
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-11-01 14:56:03 +0000

    Fix time slot reservation validation

--- website/forms.py
+++ website/forms.py
@@ -1,5 +1,6 @@
-from .models import Reservation, Table
+from .models import Reservation, Table, TIME_SLOTS
 from django import forms
+from django.utils import timezone
 
 class ReservationForm(forms.ModelForm):
     class Meta:
@@ -18,6 +19,14 @@ class ReservationForm(forms.ModelForm):
     def save(self, commit=True):
         reservation = super(ReservationForm, self).save(commit=False)
         reservation.user = self.user
+
+        time_mapping = dict(TIME_SLOTS)
+
+        if reservation.reservation_time in time_mapping:
+            start_time_str, ent_time_str = time_mapping[reservation.reservation_time].split(' - ')
+            reservation.start_time = timezone.datetime.strptime(start_time_str, '%H:%M').time()
+            reservation.end_time = timezone.datetime.strptime(ent_time_str, '%H:%M').time()
+
         if commit:
             reservation.save()
         return reservation
